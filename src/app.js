5547b3411c2e81d416efa2813a4a913d3d8c19c0
Kyle van Til <snowrider_kyle@hotmail.com>
2016-05-31 17:48:43 +0200

    Created home page and made a login form, made a link to registration page that i am gonna build now

--- src/app.js
+++ src/app.js
@@ -1,38 +1,59 @@
+//requiring modules
 var Sequelize = require ('sequelize');
 var express = require ('express');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+var session = require ('express-session');
 
 
-
-var sequelize = new Sequelize ('Blog_app', 'postgres', kyle1993 ,{
+//connect to database blog_app
+var sequelize = new Sequelize ('blog_app', 'postgres', 'kyle1993' ,{
   host: 'localhost',
   dialect: 'postgres',
-  define: {
-    timestamps: true
-  }
 });
-
+//Creating a new table called user
 var User = sequelize.define('user', {
   name: Sequelize.STRING,
   email: Sequelize.STRING,
   password: Sequelize.STRING
 });
+//Creating a new table called post
+var userPost = sequelize.define('message', {
+  title: Sequelize.STRING,
+  message: Sequelize.STRING
+});
+// Made sure User can have multiple messages but a message posted can have only one user
+User.hasMany(userPost);
+userPost.belongsTo(User);
 
 var app = express();
 
-app.use (express.static( 'resources' ));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.set('view engine', 'ejs');
+app.use (session({
+    secret: 'secret secret',
+    resave: true,
+    saveUninitialized: false
+
+}));
+
+
 app.set('views', 'src/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  console.log('Index is loaded to localhost')
-	res.render('index');
+app.get('/', function ( req, res ){
+  console.log('Index is displayed on localhost')
+	res.render('index')
+});
+
+app.get('/newUser', function ( req, res ){
+  console.log('Registration page is displayed on localhost')
+	res.render('newUser')
 });
 
 
 
-app.listen(3000, function() {
-	console.log( ' Your cookies are ready on port 3000 ' )
+sequelize.sync().then(function () {
+	var server = app.listen(3000, function () {
+		console.log('Example app listening on port: ' + server.address().port);
+	});
 });
