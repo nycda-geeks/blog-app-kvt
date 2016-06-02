b0365f7ceda577729816c2933171a84b45d6c894
Kyle van Til <snowrider_kyle@hotmail.com>
2016-06-02 16:13:49 +0200

    Made the messages display for single user and made page that displays all messages

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var session = require ('express-session');
 var pg = require ( 'pg' );
 var app = express();
+var fs = require ('fs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -13,7 +14,7 @@ var sequelize = new Sequelize ('blogdata', 'postgres', 'kyle1993' ,{
   host: 'localhost',
   dialect: 'postgres',
 });
-
+//start session
 app.use (session({
     secret: 'secret secret',
     resave: true,
@@ -47,38 +48,47 @@ userComment.belongsTo(User);
 userComment.belongsTo(Message);
 
 
-
+//set view engine and views
 app.set('views', 'src/views');
-app.set('view engine', 'ejs');
-
+app.set('view engine', 'pug');
+//display index page
 app.get('/', function ( req, res ){
   console.log('Index is displayed on localhost');
 	res.render('index');
 });
-
+//verify logindata user
 app.post('/login', function ( req, res ) {
+  if (req.body.loginEmail.length === 0){
+		res.redirect('/?message=' + encodeURIComponent(("Please fill out your username.")));
+		return;
+	}
+	if (req.body.loginPassword.length === 0){
+		res.redirect('/?message=' + encodeURIComponent(("Please fill out your password.")));
+		return;
+	}
   User.findOne({
     where: {
           email : req.body.loginEmail
         }
   }).then(function (user){
-    if ( user !== null && req.body.userPassword === user.loginPassword){
+    if ( user !== null && req.body.loginPassword === user.password){
       req.session.user = user;
       console.log(user.id)
-      res.redirect( '/profile')
+      res.redirect('/profile');
     } else {
+      res.redirect ('newUser')
       console.log('nope no user found with that combination')
 
     }
   })
 
 })
-
+// display registration page
 app.get('/newUser', function ( req, res ){
   console.log('Registration page is displayed on localhost');
 	res.render('newUser');
 });
-
+// push user info to database
 app.post('/register', function( req, res ){
   User.create({
     userName: req.body.userName,
@@ -88,17 +98,26 @@ app.post('/register', function( req, res ){
     res.render('profile');
 });
 });
+// display profile with all messages posted by user
+app.get('/profile', function(req, res){
+  var user = req.session.user;
+  console.log(user)
+  if ( user === undefined){
+    res.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+  } else {
+  Message.findAll({
+    where:{ userId: user.id}
+  }).then(function(messages){
+    res.render('profile', {
+        posts: messages
 
-app.get("/profile", function( req, res){
-  User.findOne({
-    where: {
-          id: req.session.user.id
-    }
+
+    })
+
   })
-  console.log('Profile page is displayed on localhost');
-  res.render('profile');
+}
 });
-
+//
 app.post('/profile', function ( req, res) {
     console.log(req.session.user);
   User.findOne({
@@ -110,16 +129,22 @@ app.post('/profile', function ( req, res) {
        title: req.body.title,
        message: req.body.message
      })
-    //  Message.create({
-    //    title: req.body.Title,
-    //    message: req.body.Message
-    //  })
   }).then(function () {
     res.redirect('/profile')
   })
 
 })
 
+app.get('/allPosts', (req, res)=>{
+  Message.findAll({
+
+  }).then(function(messages){
+    res.render('allPosts',{
+      posts: messages
+    }
+  )
+  })
+})
 
 sequelize.sync({force: false}).then(function () {
 	var server = app.listen(3000, function () {
